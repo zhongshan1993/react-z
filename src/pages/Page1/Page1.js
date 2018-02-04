9fe93a0a33def2e974eadb9b6f821e598fbafc37
钟山 <940817@nd.com>
2018-02-04 16:42:29 +0800

    使用 css module

--- src/pages/Page1/Page1.js
+++ src/pages/Page1/Page1.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react'
 
-import './Page1.css'
+import style from './Page1.css'
 import icon from './images/+1.png'
 
 export default class Page1 extends Component {
   render() {
     return (
-      <h1 className='page-box'>
+      <h1 className={style['page-box']}>
         this is page1!
         <img src={icon} />
       </h1>
